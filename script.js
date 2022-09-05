c985eab889949dc9a3308a08ad2529c8a7a30bce
mrtfe <105384042+mrtfe@users.noreply.github.com>
2022-09-05 21:59:49 +0300

    feat: :tada: grid created

--- script.js
+++ script.js
@@ -1,1 +1,24 @@
 console.log("script is loaded");
+
+const BOX = document.querySelector(".sketch-box__container");
+const square = document.querySelectorAll(".one-grid");
+
+function createGrid() {
+  for (i = 0; i < 256; i++) {
+    const square = document.createElement("div");
+    BOX.appendChild(square);
+    square.style.border = "1px solid black";
+    square.style.width = "20px";
+    square.style.height = "20px";
+    square.style.backgroundColor = "white";
+    square.classList.add = "one-grid";
+  }
+}
+
+createGrid();
+
+console.log(square);
+
+// square.addEventListener("mouseover", (e) => {
+//   square.style.backgroundColor = "grey";
+// });
